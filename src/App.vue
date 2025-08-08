114df7be65583fc2074e0d948ea8fd1f8976df85
TraumDE <eugene301workmaik@gmail.com>
2025-08-08 23:31:57 +0300

    Add work completed tasks counter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import TaskForm from './components/TaskForm.vue'
 import type { Task } from './types'
 import TasksList from './components/TasksList.vue'
 
 const message = ref('Task App')
 const tasks = ref<Task[]>([])
+
+const totalDone = computed(() =>
+  tasks.value.reduce((total, task) => (task.done ? total + 1 : total), 0),
+)
 
 const addTask = (newTask: string): void => {
   tasks.value.push({
@@ -27,7 +31,7 @@ const toggleTaskDone = (id: string): void => {
     <h1>{{ message }}</h1>
     <task-form @add-task="addTask" />
     <h3 v-if="!tasks.length">Add task to get started</h3>
-    <h3 v-else>x / {{ tasks.length }} tasks completed</h3>
+    <h3 v-else>{{ totalDone }} / {{ tasks.length }} tasks completed</h3>
     <!-- <h3>There are {{ tasks.length }} tasks</h3> -->
     <tasks-list :tasks @toggle-done="toggleTaskDone" />
   </main>
